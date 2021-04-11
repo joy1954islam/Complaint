e1ddf79da590038db8f5df65df4bb55d60478729
joyislam1954 <joyislam1954@gmail.com>
2021-04-11 20:19:49 +0600

    create app public user and change settings file

--- Complaint/Complaint/settings.py
+++ Complaint/Complaint/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
     'SuperAdmin',
+    'public_user.apps.PublicUserConfig',
 
     'django.contrib.admin',
     'django.contrib.auth',
