97a69bb54f7d845519454f7febbf58054537e121
joyislam1954 <joyislam1954@gmail.com>
2021-04-11 21:07:09 +0600

    Create Model Complaint in public user

--- Complaint/public_user/models.py
+++ Complaint/public_user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
+from SuperAdmin.models import Ministry
 
-# Create your models here.
+
+class Complaint(models.Model):
+    username = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    ministry_name = models.ForeignKey(Ministry, on_delete=models.CASCADE, verbose_name='Ministry Name')
+    location = models.TextField(verbose_name='Location')
+    image = models.ImageField(verbose_name='Location Image')
+    note = models.TextField(verbose_name='Note')
+
+    def __str__(self):
+        return self.username
