1698d64e687194ba8add2fc7309d4e90f93b117e
joyislam1954 <joyislam1954@gmail.com>
2021-04-12 19:16:07 +0600

    Backend & Fronted Include Complaint List function For Ministry Incharge

--- Complaint/ministry_incharge/urls.py
+++ Complaint/ministry_incharge/urls.py
@@ -4,5 +4,6 @@ from ministry_incharge import views
 
 urlpatterns = [
     path('home/', views.ministry_incharge_home, name='ministry_incharge_home'),
+    path('complaint/list/', views.complaint_list, name='complaint_list'),
 
 ]
--- Complaint/ministry_incharge/views.py
+++ Complaint/ministry_incharge/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-
-# Create your views here.
+from public_user.models import Complaint
 
 
 def ministry_incharge_home(request):
     return render(request, 'Ministry Incharge/Home.html')
+
+
+def complaint_list(request):
+    complaint = Complaint.objects.all()
+    context = {
+        'complaint': complaint
+    }
+    return render(request, 'Ministry Incharge/Complaint/complaint_list.html', context=context)
 
