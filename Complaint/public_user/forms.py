40f4d926b7b16bc12c7ab7779045e9cac154a98b
joyislam1954 <joyislam1954@gmail.com>
2021-04-18 19:56:52 +0600

    fixed bugs in create complaint

--- Complaint/public_user/forms.py
+++ Complaint/public_user/forms.py
@@ -3,8 +3,7 @@ from .models import *
 
 
 class ComplaintForms(forms.ModelForm):
-    ministry_name = forms.CharField(disabled=True)
 
     class Meta:
         model = Complaint
-        fields = ['ministry_name', 'location', 'image', 'note']
+        fields = ['location', 'image', 'note']
--- Complaint/public_user/views.py
+++ Complaint/public_user/views.py
@@ -9,16 +9,20 @@ def create_complaint(request, complaint_id):
     complaint = Ministry.objects.get(id=complaint_id)
     form = ComplaintForms(instance=complaint)
     if request.method == "POST":
-        form = ComplaintForms(request.POST or None, request.FILES or None, instance=complaint)
+        form = ComplaintForms(request.POST or None, request.FILES or None)
         if form.is_valid():
+            print('valid form')
             c = form.save(commit=False)
             c.username = request.user
+            c.ministry_name = complaint
             c.save()
             return redirect(reverse('home'))
         context = {
             'form': form,
+            'complaint': complaint
         }
         return render(request, 'create_complaint.html', context=context)
+
     if request.method == "GET":
         context = {
             'form': form,
