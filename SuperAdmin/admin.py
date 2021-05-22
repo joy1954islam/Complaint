ff53f0696d19e584f2023becc62aedc690b0c63d
joyislam1954 <joyislam1954@gmail.com>
2021-05-22 20:33:25 +0600

    create models District and PolicStation

--- SuperAdmin/admin.py
+++ SuperAdmin/admin.py
@@ -2,3 +2,5 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Ministry)
+admin.site.register(District)
+admin.site.register(PoliceStation)
--- SuperAdmin/models.py
+++ SuperAdmin/models.py
@@ -4,6 +4,21 @@ from django.db import models
 from django.urls import reverse
 
 
+class District(models.Model):
+    district = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.district
+
+
+class PoliceStation(models.Model):
+    district = models.ForeignKey(District, on_delete=models.CASCADE)
+    police_station = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.police_station
+
+
 class Ministry(models.Model):
     username = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     ministry_name = models.CharField(max_length=100, verbose_name='Ministry Name')
