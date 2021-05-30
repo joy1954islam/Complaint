f6765f1756242ad740a8813c3d8de6f1eb601cd1
joyislam1954 <joyislam1954@gmail.com>
2021-05-30 20:47:37 +0600

    remove settings files in mail host user

--- Complaint/settings.py
+++ Complaint/settings.py
@@ -141,7 +141,7 @@ MAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'joyislam1954@gmail.com'
+EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 
 ENABLE_USER_ACTIVATION = True
